8d6a2a56e401234f760a400e63a725f24275d89b
csy <476234335@qq.com>
2020-12-09 16:24:56 +0800

    update bash

--- ubuntu_1804/config_file_update.sh
+++ ubuntu_1804/config_file_update.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+# shell
 cp ~/.bashrc ./bashrc/
 cp ~/.zshrc ./zshrc/
 
-
+# vim
 cp ~/.vimrc ./vim/
 cp ~/.vim/.ycm_extra_conf.py ./vim/YCM/
 
-
+# tmux
 cp ~/.tmux.conf ./tmux/
+
+# alacritty
 cp ~/.config/alacritty/alacritty.yml ./alacritty/
+
+# espanso
 cp ~/.config/espanso/default.yml ./espanso/
 
-
+# shadowsocks
 cp /etc/shadowsocks/config.json ./shadowsocks/
 cp ~/Desktop/JMS.sh ./shadowsocks/
+
+# taskwarrior
+# html file
